5d11ab10a05e87f1d9f0d829a86793fcb8c1e583
Igor Khotin <khotin@gmx.com>
2021-04-22 00:50:44 +0300

    [+] fade in/out transition for skills chart

--- mod/dna/hud/Skills.js
+++ mod/dna/hud/Skills.js
@@ -2,6 +2,7 @@ const df = {
     hidden: true,
     rw: .5,
     rh: .5,
+    fadeTime: .5,
 }
 
 class Skills {
@@ -16,12 +17,48 @@ class Skills {
         this.y = ry(.4) - this.h/2
     }
 
+    show() {
+        this.hidden = false
+        this.fadeIn = this.fadeTime
+    }
+
+    hide() {
+        this.fadeOut = this.fadeTime
+    }
+
     touch() {
-        this.hidden = !this.hidden
+        if (this.fadeIn > 0 || this.fadeOut > 0) return
+        if (this.hidden) this.show()
+        else this.hide()
+    }
+
+    evo(dt) {
+        if (this.hidden) return
+        if (this.fadeIn > 0) {
+            this.fadeIn -= dt
+            if (this.fadeIn <= 0) this.fadeIn = 0
+        }
+        if (this.fadeOut > 0) {
+            this.fadeOut -= dt
+            if (this.fadeOut <= 0) {
+                this.fadeOut = 0
+                this.hidden = true
+            }
+        }
     }
 
     draw() {
+        save()
+        if (this.fadeIn > 0) {
+            alpha(1 - this.fadeIn/this.fadeTime)
+        }
+        if (this.fadeOut > 0) {
+            alpha(this.fadeOut/this.fadeTime)
+        }
+
         blocky()
         image(res.paper, this.x, this.y, this.w, this.w)
+
+        restore()
     }
 }
